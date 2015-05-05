14c8922b2201a0e48007248863c0fa467933e423
Andy Baek <andybaek95@gmail.com>
2015-05-05 00:09:27 -0400

    Base working

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+#!/usr/bin/env node
 // Libraries.
 var express = require('express');
 var path = require('path');
 var http = require('http');
 var app = express();
-var server = http.Server(app),
+var debug = require('debug')('nba-playoff-bracket');
 
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 // Render landing page.
 app.get('/', function(req, res) {
@@ -16,7 +17,8 @@ app.get('/', function(req, res) {
 });
 
 // Port listening.
-var port = process.env.PORT || 5000;
-server.listen(port, function() {
-  console.log ('Started listening on port: ' + port);
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log('Started listening on port: ' + server.address().port);
+  debug('Express server listening on port ' + server.address().port);
 });
--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,10 @@
-var myRatings = angular.module('myRatings', ['ngRoute', 'ui.bootstrap'])
+var myRatings = angular.module('myRatings', ['ngRoute'])
   .config(['$routeProvider',
     function($routeProvider) {
       $routeProvider
         .when('/product/:name?/:category?', {
           templateUrl: '/views/product.html',
           controller: 'ProductController'
-        })
-        .when('/addRating/:name/:category', {
-          templateUrl: '/views/addRating.html',
-          controller: 'AddRatingController'
         })
         .otherwise({
           redirectTo: '/product'
