8f684a7725d6bc24aa0b04389a422dbacd04322a
Andy Baek <andybaek95@gmail.com>
2015-05-05 17:23:09 -0400

    removed bad code

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var routes = require('./routes/index');
+var debug = require('debug')('nba-playoff-bracket');
 
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '')));
 
-app.use('/', routes);
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
 
 module.exports = app;
