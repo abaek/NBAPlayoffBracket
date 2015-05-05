c6436456a49f35e887e3ca2ae4061247f0168474
Andy Baek <andybaek95@gmail.com>
2015-05-05 00:27:29 -0400

    added bodyParser

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '')));
 
 // Render landing page.
